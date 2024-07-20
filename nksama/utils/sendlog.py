016810bfbbd6f12a1b85fde0d027ebc3ea16e9b3
Dark souls <87437573+Niyamatop@users.noreply.github.com>
2024-07-20 19:48:08 +0530

    Update sendlog.py

--- nksama/utils/sendlog.py
+++ nksama/utils/sendlog.py
@@ -2,4 +2,4 @@ from nksama import bot
 
 
 def send_log(err, module):
-    bot.send_message(-1001646296281, f"error in {module}\n\n{err}")
+    bot.send_message(-1002239117016, f"error in {module}\n\n{err}")
